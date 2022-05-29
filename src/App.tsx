a73394781a2aa67e50c2a311b2b92e78234ed76c
Viraj Shah <shahvirajd@gmail.com>
2022-05-28 23:04:29 -0400

    🎨 Created the UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './tsw.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div id='container'>
+            <img src={logo} alt='Soggy Waffle' width='300' />
+            <div id='menu'>
+                <button>Fullscreen</button>
+                <button>Popup</button>
+                <button>Alert</button>
+                <button>Confirm</button>
+                <button>Prompt</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
